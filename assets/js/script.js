d9b71315de47cae9c443b2f78f152d24ac6174f3
Valentina Toledo <valentina.toledoporma@gmail.com>
2017-08-10 12:26:23 -0400

    añado js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $.ajax({
+      url: 'https://netflixroulette.net/api/api.php?actor=' + 'john travolta',
+     type: 'GET',
+     dataType: 'json',
+   })
+    .done(function(e) {
+    console.log(e);
+
+      e.forEach(function(el){
+        var contenido = '<div><h6>' + el.show_title + '</h></div>';
+         var act = $(".nomActor").append(contenido);
+       })
+
+    })
+
+    .fail(function() {
+     console.log("error");
+   });
+
+})
