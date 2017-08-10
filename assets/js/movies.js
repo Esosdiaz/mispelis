7fce51f59145b90a3d2bb789f40b27bed4442352
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-08-10 13:29:45 -0400

    guarda nombre pelicula elegida en local storage

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var pelis = ['As Good as It Gets', 'Vampire Knight', 'The Longest Day', 'Arthur and the Invisibles','The French Connection', 'Love Actually', 'The Union: The Business Behind Getting High', 'Marathon', 'A Greater Yes: The Story of Amy Newhouse'];
-	
+	var cont = 0;
 	pelis.forEach(function(val){
 		$.ajax({
 			url: 'https://netflixroulette.net/api/api.php',
@@ -26,10 +26,34 @@ $(document).ready(function() {
 		var directorMovies = $('<p>').text(arr.director);
 
 		var tituloMovies = $('<div>').attr('class', 'titulo-movies');
+			tituloMovies.attr('id', 'movies-'+ cont);
 			tituloMovies.append(nombrePeliculaMovies);
 			tituloMovies.append(anoLanzamientoMovies);
 			tituloMovies.append(categoriaMovies);
 
-		$('.movies').append(tituloMovies);
+		var detalleMovies = $('<div>').attr('class', 'detalle-movies');
+			detalleMovies.append(duracionMovies);
+			detalleMovies.append(directorMovies);
+
+		var guardarFavoritoMovies = $('<button>').attr('class', 'guardar-favorito-movies');
+			guardarFavoritoMovies.text('Guardar a Favoritos');
+
+		var infoMovies = $('<div>').attr('class', 'info-movies');
+			infoMovies.append(tituloMovies);
+			infoMovies.append(detalleMovies);
+			infoMovies.append(guardarFavoritoMovies);
+
+		$('.movies').append(infoMovies);
+
+		$( "#movies-"+cont ).click(function() {
+  			alert( arr.show_title);
+  			localStorage.setItem("tituloPelicula", arr.show_title);
+		});
+
+		cont++;
+		console.log(cont);
 	}
+
+
+	//var valuee = localStorage.getItem(tituloPelicula");
 });
