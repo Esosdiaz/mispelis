6ee956210799b53f0d68f7136c56d3b8a5b0d5a4
esosdiaz <diazgonzalez.perla@gmail.com>
2017-08-11 03:02:00 -0300

    validaciones create account completadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,18 +14,63 @@ $(document).ready(function(){
 	function validateForm(){
 		var usuario = $('#user').val();
 		var contrasena = $('#password').val();
-		var numReg = /^[0-9]/;
 
 		if (usuario == ""){
 			$('#mensajeUser').fadeIn();
         	}else{
-        		if(contrasena == "" || contrasena.length > 8 || !numReg.test(contrasena) ){
+        		if(contrasena == ""){
         		$('#mensajePass').fadeIn();
         		}else{
 				window.location.href = "xxxx.html"; //redirecciona una vez que se cumplan las validaciones. 
 				}	
         	}
         }
+
+    //validaciones create account
+    $('#mnsNameReg').hide();
+	$('#mnsUserReg').hide();
+	$('#mnsPassReg').hide();
+	$('#mnsMailReg').hide();
+	$('#mnsCountryReg').hide();
+
+	$('#btn-create').click(function(){
+		validateFormAccount();
+	});
+
+	function validateFormAccount(){
+		var nombreReg = $('#nameReg').val();
+    	var usuarioReg = $('#userReg').val();
+		var contrasenaReg = $('#passwordReg').val();
+		var correoReg = $('#mailReg').val();
+		var paisReg = $('#countryReg').val();
+
+		var mailRegr = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
+
+		if (nombreReg == ""){
+			$('#mnsNameReg').fadeIn();
+			}else{
+        		if(usuarioReg == "" ){
+        			$('#mnsUserReg').fadeIn();
+        			}else{
+        				if(contrasenaReg == ""){
+        					$('#mnsPassReg').fadeIn();
+        					}else{
+        						if(correoReg == "" || !mailRegr.test(correoReg)){
+        						$('#mnsMailReg').fadeIn();
+        						}else{
+        							if(paisReg == ""){
+        								$('#mnsCountryReg').fadeIn();
+        								}else{
+        								window.location.href = "xxxx.html"
+        							}
+        						}
+        					}
+						}
+
+				}
+			}
+
+
     });      				
 
 
