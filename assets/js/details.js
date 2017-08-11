8d1be531a5829828f7af06a7ba0631f528063ac5
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-10 22:22:46 -0400

    Estilos imágenes agregadas

--- assets/js/details.js
+++ assets/js/details.js
@@ -15,12 +15,16 @@ $(document).ready(function() {
 				console.log("error");
 			})
 	function mostrar(arr){
+		//Imagen en el div vacio
+		var imagenDetails = $('<img>').attr("src",arr.poster);
+		$(".imagen-pelicula-details").append(imagenDetails);
 		var nombrePeliculaMovies = $('<p>').text(arr.show_title);
 		var anoLanzamientoMovies = $('<p>').text(arr.release_year);
 		var categoriaMovies = $('<p>').text(arr.category);
 		var duracionMovies = $('<p>').text(arr.runtime);
 		var directorMovies = $('<p>').text(arr.director);
-		var imagenDetails = $('<img>').attr("src",arr.poster);
-		$(".imagen-pelicula-details").append(imagenDetails);
+		var sinopsisMovies = $('<p>').text(arr.summary);
+		show_cast
+
 	}
 });
