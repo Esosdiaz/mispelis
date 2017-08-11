be33609c42dc6803eea0e671aebc786bbc21ba61
Valentina Toledo <valentina.toledoporma@gmail.com>
2017-08-11 10:45:15 -0400

    ultimos cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
+     $(".button-collapse").sideNav();
+  var actorElegido = localStorage.getItem("actor");
 
-  //var actorElegido = localStorage.getItem("nombredeActorElegido");
-
-    $.ajax({
-      url: 'https://netflixroulette.net/api/api.php?actor=' + 'john travolta' /*+ actorElegido*/ ,
-     type: 'GET',
-     dataType: 'json',
-   })
-    .done(function(e) {
+  $.ajax({
+    url: 'https://netflixroulette.net/api/api.php?actor=' + actorElegido ,
+    type: 'GET',
+    dataType: 'json',
+  })
+  .done(function(e) {
     console.log(e);
 
-      e.forEach(function(el){
-        var contenido = '<div><h6>' + el.show_title + '</h6></div><div><p>'+ 'Categoria: ' + el.category + '</p></div><hr>';
-         var act = $(".pelisActor").append(contenido);
-       })
-
+    e.forEach(function(el){
+      var contenido = '<div><h6>' + el.show_title + '</h6></div><div><p>'+ 'Categoria: ' + el.category + '</p></div><hr>';
+      var act = $(".pelisActor").append(contenido);
     })
-    .fail(function() {
-     console.log("error");
-   })
+
+  })
+  .fail(function() {
+   console.log("error");
+ })
+
 
 });
+
+
